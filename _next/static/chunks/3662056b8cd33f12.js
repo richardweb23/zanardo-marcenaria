e5b8278dab1269c14a0fc2254a32561731d1be13
Richard Quintanilha <richardweb23@gmail.com>
2025-10-04 10:01:39 -0300

    Updates

--- _next/static/chunks/3662056b8cd33f12.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/4666d8cd025f5229.js",
-  "static/chunks/turbopack-d83c98159a54644d.js"
-])
--- _next/static/chunks/e97677efb7b770fa.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/4666d8cd025f5229.js",
-  "static/chunks/turbopack-3a94dc583c91fd56.js"
-])
