d0ea9bf60594c7edf720b10e2e1a766990cf7e12
Richard Quintanilha <richardweb23@gmail.com>
2025-10-06 16:10:00 -0300

    Ajuste caminhos

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export', // ativa exportação estática
-  basePath: isProd ? '/zanardo-marcenaria' : '', // necessário para GitHub Pages
-  assetPrefix: isProd ? '/zanardo-marcenaria/' : '', // corrige caminhos de assets
   images: {
     unoptimized: true, // necessário se usar next/image
   },
